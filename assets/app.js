deb42afae7b84b56c3eb20c8e56c3fc3494258ff
Ximena Gonzalez <xigonzalez28@gmail.com>
2022-05-02 12:01:39 +0200

    devinette

---
+++ assets/app.js
@@ -0,0 +1,11 @@
+// generar un numero
+
+// verificar si el nombre es par o impar antes de todo, divido un numero par por un impar, 
+// calculer le rest d'un division sans vergule
+
+// escrbir el mensaje
+
+let randomNum = document.querySelector('#randomNumInput');
+
+// randomNum = Math.floor(math.random()*21);
+
